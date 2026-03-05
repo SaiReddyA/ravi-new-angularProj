1e1955dc8d51f385f7edeff9c6f468e62c27aa20
Sai Reddy Local <saireddy1m43@gmail.com>
2026-03-05 21:12:56 +0530

    Lazy loading and Routing

--- ravi-ang-proj/src/app/app.routes.ts
+++ ravi-ang-proj/src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { EmployeeForm } from './Components/employee-form/employee-form';
 import { EmployeeList } from './Components/employee-list/employee-list';
 
 export const routes: Routes = [
-    { path: 'employeeForm', component: EmployeeForm }, 
-    { path: 'employeeList', component: EmployeeList }, 
+    { path: 'employeeform', component: EmployeeForm }, 
+    { path: 'employeelist', component: EmployeeList }, //Egar loading implementation
+    { path:'studentform', loadComponent: () => import('./Components/student-form/student-form').then(m => m.StudentForm)
+    }
 ];
+    
